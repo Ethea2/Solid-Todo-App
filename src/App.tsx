a885fde57c5d295e0fc91723ca74f14c156944bf
Nathan Andres <85412743+Ethea2@users.noreply.github.com>
2022-03-09 15:10:10 +0400

    done

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { createSourceEventStream } from 'graphql';
 import type { Component } from 'solid-js'
 import { createSignal, For, createResource } from 'solid-js'
 import { fetchTodoData, insertTodoData, removeTodoData } from './graphieql'
-import { removeItem } from './util';
+import { CommandPalette } from './keys'
 
 
 interface TodoItem {
@@ -14,15 +14,15 @@ interface TodoItem {
 
 const App: Component = () => {
   const [todoList, setTodoList] = createSignal<TodoItem[]>([])
-  const [todo, {mutate, refetch}] = createResource(todoList, fetchTodoData)
+  const [todo, { mutate, refetch }] = createResource(todoList, fetchTodoData)
   let inputBox
-  fetchTodoData().then(setTodoList)
+  fetchTodoData().then(setTodoList) //ask lyon peeps
   const addTodo = (label) => {
-    if(label.trim() === "") {
+    if (label.trim() === "") {
       console.log("nothing here")
     } else { //make adding and removing reactive!!!
-      insertTodoData(false ,label)
-      setTodoList((currentList) => [...currentList, {id: todo()[currentList.length], label, is_complete: false}])
+      insertTodoData(false, label)
+      setTodoList((currentList) => [...currentList, { id: todo()[currentList.length], label, is_complete: false }])
       console.log(todoList())
     }
   }
@@ -35,47 +35,51 @@ const App: Component = () => {
 
   const deleteData = (label) => {
     removeTodoData(label)
-    removeItem(todoList(), label) //make removing items reactive
-    //console.log(newSet)
+    //inItem(todoList(), label) //make removing items reactive
+    setTodoList((currentList) => currentList.filter((todoObject) => !Object.values(todoObject).includes(label)))
   }
-
   let input: string
   return (
     <>
-    <div>
-      <p class="text-4xl text-center p-8 font-semibold">Basic Todo App</p>
-    </div>
-    <div class="flex">
-      <div class="flex-1 shrink w-64">
-        <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 
+      <CommandPalette />
+      <div>
+        <p class="text-4xl text-center p-8 font-semibold">Basic Todo App</p>
+      </div>
+      <div class="flex">
+        <div class="flex-1 shrink w-64">
+          <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 
         rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none 
         focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-        placeholder="Add tasks..." type="text" name="add" ref={inputBox}></input>
-        <ul>
-          <For each={todoList()}>{(item, i) =>
-            <div class="flex">
-              <div class="flex-auto w-full">
-                <li class="text-indigo-500 text-center m-2.5 text-3xl bg-slate-200 cursor-pointer font-semibold" onclick={(e)=> {toggleTodo(item.id)
-                console.log("Item toggled.", item.is_complete)}}
-                className={!item.is_complete ? "no-underline" : "line-through decoration-pink-500 italic"}>
-                  {item.label}
-                </li>
+            placeholder="Add tasks..." type="text" name="add" ref={inputBox}></input>
+          <ul>
+            <For each={todoList()}>{(item, i) =>
+              <div class="flex">
+                <div class="flex-auto w-full">
+                  <li class="text-indigo-500 text-center m-2.5 text-3xl bg-slate-200 cursor-pointer font-semibold" onclick={(e) => {
+                    toggleTodo(item.id)
+                    console.log("Item toggled.", item.is_complete)
+                  }}
+                    className={!item.is_complete ? "no-underline" : "line-through decoration-pink-500 italic"}>
+                    {item.label}
+                  </li>
+                </div>
+                <div class="flex-auto w-24">
+                  <button class="px-8 bg-teal-400 rounded-lg w-full h-10 mt-2" onclick={(e) => { deleteData(item.label) }}><img src="https://img.icons8.com/ios-glyphs/30/000000/delete-forever.png" />
+                  </button>
+                </div>
               </div>
-              <div class="flex-auto w-24">
-                <button class="px-8 bg-teal-400 rounded-lg w-full h-10 mt-2" onclick={(e) => {deleteData(item.label)}}><img src="https://img.icons8.com/ios-glyphs/30/000000/delete-forever.png"/>
-                </button>
-              </div>
-            </div>
-          }</For>
-        </ul>
-      </div>  
-      <div class="flex-2 w-64 shrink">
-        <button class="bg-indigo-500 rounded-lg w-full h-10"
-          onclick={(e) => {addTodo(inputBox.value)
-            inputBox.value = ""}}>Hello</button>
+            }</For>
+          </ul>
+        </div>
+        <div class="flex-2 w-64 shrink">
+          <button class="bg-indigo-500 rounded-lg w-full h-10"
+            onclick={(e) => {
+              addTodo(inputBox.value)
+              inputBox.value = ""
+            }}>Hello</button>
+        </div>
       </div>
-    </div>
-   </>
+    </>
   )
 }
 
--- src/util.tsx
+++
@@ -1,9 +0,0 @@
-
-export function removeItem(arr: object[], item: string) {
-    arr.forEach((element, i) =>{
-        if(Object.values(element).includes(item)) {
-            arr.splice(i,1)
-        }
-    })
-    return arr
-}
